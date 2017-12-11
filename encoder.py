e114a51c1e875b5b56a243b39002703f41cbb4f8
Harshita Sahijwani <harshita.rygbee@gmail.com>
2017-12-11 15:28:00 -0500

    Change encoder.py

--- encoder.py
+++ encoder.py
@@ -1,7 +1,6 @@
 import pandas as pd
-import pickle
 
-def read_csv(filename, sep=",", header=0, missing=""):
+def read_csv(filename, sep=",\s*", header=0, missing=""):
     df = pd.read_csv(filename, sep=sep, header=header, na_values=missing, engine='python')
     return df
 
@@ -9,11 +8,11 @@ def read_songs(filename='songs.csv'):
     print('Reading', filename)
     df = read_csv(filename)
     songid_lst = df['song_id'].values.tolist()
-     
+    df = df.set_index('song_id')
     df['genre_ids'].fillna('-1', inplace=True)
     df['language'].fillna('-1', inplace=True)
     df['artist_name'].fillna('Various Artists', inplace=True)
-    df.drop(['song_id', 'song_length', 'lyricist', 'composer'], axis=1, inplace=True)
+    df.drop(['song_length', 'lyricist', 'composer'], axis=1, inplace=True)
  
     return df, songid_lst
 
@@ -21,12 +20,16 @@ def df_genre(df,N=200):
     genres = df['genre_ids'].value_counts()[:N]
     genres = list(genres.index)
     df = df.sort_values('artist_name', axis=0)
+    indexes = df.index.values
     columns = [i for i in genres]
     columns.append('language')
     new_df = pd.DataFrame(columns=columns)
     for artist in df['artist_name'].unique():
         cur_df = df.loc[df['artist_name'] == artist]
         new_df = new_df.append(artist_by_genre(cur_df,genres))
+    indexes = pd.DataFrame(indexes,columns=['index'])
+    new_df = new_df.assign(index=indexes.values)
+    new_df= new_df.set_index('index')
     return new_df
 
 def artist_by_genre(df,genres):
@@ -57,10 +60,7 @@ def artist_by_genre(df,genres):
             a = a.join(probs)
         res = res.append(a)
     return res
-    
 
-df,sid = read_songs('songs.csv')
-df = df_genre(df)  
-# df
-filename = 'artist_by_genre.df'
-pickle.dump(df, open(filename,'wb'))
+df,sid = read_songs('songs_train.csv')
+df = df_genre(df,20)  
+df.to_csv('train_out.csv')
